40be7ddcfd1ef255e179ece995308b6679e73525
Laakiin <anakinmrq@gmail.com>
2021-12-17 15:20:47 +0100

    jpp

--- bashVer/autotraceroute.sh
+++ bashVer/autotraceroute.sh
@@ -4,7 +4,9 @@ LBLU='\033[1;34m'
 YEL='\033[1;33m'
 NC='\033[0m'
 
-j=0
+declare -a files
+
+j=1
 
 FILE="/tmp/out.$$"
 GREP="/bin/grep"
@@ -23,24 +25,17 @@ then
     then
         for i in {"-n -I","-n -T -p 80","-n -T -p 443","-n -U -p 53","-n -U -p 123"}
         do
-            k=$j
-            #j=$j+1
+            files[$j]=".addrTmp_$j"
+            echo -e "${LBLU}########## Options: $i ##########${NC}"
+            traceroute $i $1 | cut -c5-20 | cut -d'.' -f 1,2,3,4 | cut -d" " -f 1 | sed -e "s/eroute/\r/g" >> ${files[$j]}
             let "j = j+1"
-            echo $j
-            echo -e "${LBLU}########## Options: $i ##########${NC}" >> .addrTmp_$j
-            traceroute $i $1 | cut -c5-20 | cut -d'.' -f 1,2,3,4 | cut -d" " -f 1 | sed -e "s/eroute/\r/g" >> .addrTmp_$j
-            if grep -q "$1" .addrTmp_$j
-                then
-                    
-                    cat .addrTmp_${j} >> $2
-                    
-            else
-                    rm .addrTmp_${j}
-            fi
+            cat ${files[$j]} >> $2
         done
         echo -e "${YEL}Traceroute finished: ${NC}"
         cat $2
         rm .addrTmp*
+        echo -e "${YEL}${files[@]}${NC}"
+        echo -e "${LBLU}${files[11]}${NC}"
     else
         echo "You have to precise the host as the first argument and the file name in which you will save as a second argument"
     fi
