ff5e06d2d42f3444a1a79bbc17332a5cfcd68906
Laakiin <anakinmrq@gmail.com>
2022-01-07 17:14:45 +0100

    samarch

--- autotraceroute.sh
+++ autotraceroute.sh
@@ -4,6 +4,8 @@ LBLU='\033[1;34m'
 YEL='\033[1;33m'
 NC='\033[0m'
 
+declare -a colors=("aqua", "blue", "crimson", "chartreuse", "darkgoldenrod1", "darkorchid1", "deeppink", "gray1", "gray24")
+
 j=0
 
 FILE="/tmp/out.$$"
@@ -18,6 +20,9 @@ if [ -e $2 ]; then
 fi
 
 mkdir $2
+mkdir $2/.tmp
+touch $2/dotFile
+echo "digraph {" >> $2/dotFile
 
 if [[ -n $1 ]]
 then
@@ -26,16 +31,26 @@ then
         for i in {"-n -I","-n -T -p 80","-n -T -p 443","-n -U -p 53","-n -U -p 123"}
         do
             let "j = j+1"
-            echo -e "${LBLU}########## Options: $i ##########${NC}" >> .addrTmp_$j
             traceroute $i $1 | cut -c5-20 | cut -d'.' -f 1,2,3,4 | cut -d" " -f 1 | sed -e "s/eroute/\r/g" >> $2/$j.rte
-            done
+            touch $2/$j.csv
+            touch $2/.tmp/$j
+            sed  's/.*/&"->/' $2/$j.rte > $2/.tmp/$j
+            sed  's/^/"/' $2/.tmp/$j > $2/.tmp/$j.dot
+            sed -i '1d' $2/.tmp/$j.dot
+            cat $2/.tmp/$j.dot | sed s/\*//g | sed s/\"\"\-\>//g >> $2/.tmp/$j.aled
+            
+            cat $2/.tmp/$j.aled | tr -d '\n' >> $2/$j.dot
+
+        done
+        dot=$(cat $2/$j.dot)
+        echo "${dot::-2}[color=red];" >> $2/dotFile
         echo -e "${YEL}Traceroute finished: ${NC}"
         cat $2/*.rte
-        rm .addrTmp*
+        echo "}" >> $2/dotFile
+        xdot $2/dotFile
     else
         echo "You have to precise the host as the first argument and the file name in which you will save as a second argument"
     fi
 else
     echo "You have to precise the host as the first argument and the file name in which you will save as a second argument"
 fi
-
---
+++ xdot.py
@@ -0,0 +1,5 @@
+import os
+import sys
+dir=sys.argv[1]
+cmd="xdot "+dir
+os.system(cmd)
