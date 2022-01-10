596d25df848cdf240781913993d4c08c52323c2c
Laakiin <anakinmrq@gmail.com>
2022-01-10 15:20:36 +0100

    lul

--- autotracerouteFile.sh
+++ autotracerouteFile.sh
@@ -3,9 +3,11 @@
 LBLU='\033[1;34m'
 YEL='\033[1;33m'
 NC='\033[0m'
-RED='\033[0;31m'
+RED='\033[1;31m'
+GRE='\033[1;32m'
+BOLD=$(tput bold)
 
-declare -a colors=("aqua", "blue", "crimson", "chartreuse", "darkgoldenrod1", "darkorchid1", "deeppink", "gray1", "gray24")
+declare -a colors=("aqua" "blue" "crimson" "chartreuse" "darkgoldenrod1" "darkorchid1" "deeppink" "gray1" "gray24")
 
 nbElem=$(grep -o , $1 | wc -l)   
 let nbElem=nbElem+1
@@ -29,40 +31,41 @@ mkdir $2
 mkdir $2/.tmp
 touch $2/dotFile
 echo "digraph {" >> $2/dotFile
-
 color=0
-
 if [[ -n $1 ]]
 then
     if [[ -n $2 ]]
     then
         for (( f=1 ; f<=nbElem ; f++ ))
         do
-            site=$(cut -d',' -f $f)
-            
-            for i in {"-n -I","-n -T -p 80","-n -T -p 443","-n -U -p 53","-n -U -p 123"}
+            site=$(cut -d',' -f $f $1)
+            mkdir $2/$site
+            mkdir $2/.tmp/$site
+            echo -e "${YEL}     Site: $site  ${NC}"
+            for i in {"-n -I","-n -T -p 80","-n -T -p 443","-n -U -p 53","-n -U -p 123","-n -T -p 20","-n -T -p 25","-n -T -p 37","-n -U -p 37","-n -T -p 992"}
             do
-                echo -e "${LBLU} Beggining the route of $site with this options; $i ${NC}"
+                echo -e "${LBLU}        Beggining the route of $site with these options: $i ${NC} ..."
                 let "j = j+1"
-                traceroute $i $site | cut -c5-20 | cut -d'.' -f 1,2,3,4 | cut -d" " -f 1 | sed -e "s/eroute/\r/g" >> $2/$j.rte
+                traceroute $i $site | cut -c5-20 | cut -d'.' -f 1,2,3,4 | cut -d" " -f 1 | sed -e "s/eroute/\r/g" >> $2/$site/$j.rte
                 touch $2/$site/$j.csv
                 touch $2/.tmp/$j
                 sed  's/.*/&"->/' $2/$site/$j.rte > $2/.tmp/$site/$j
                 sed  's/^/"/' $2/.tmp/$site/$j > $2/.tmp/$site/$j.dot
                 sed -i '1d' $2/.tmp/$site/$j.dot
                 cat $2/.tmp/$site/$j.dot | sed s/\*//g | sed s/\"\"\-\>//g >> $2/.tmp/$site/$j.aled
-            
                 cat $2/.tmp/$site/$j.aled | tr -d '\n' >> $2/$site/$j.dot
+                echo -e "${GRE}             Done. ${NC}"
 
             done
         dot=$(cat $2/$site/$j.dot)
-        echo "${dot::-2}[color=${colors[$color]};" >> $2/dotFile
+        echo "${dot::-2}[color=${colors[$color]}];" >> $2/dotFile
+        echo -e  "${YEL} Color of $site is ${NC}${BOLD} ${colors[$color]}  ${NC} "
         let "color = color+1"
     done
-    echo -e "${YEL}Traceroute finished: ${NC}"
-    cat $2/*.rte
+    echo -e "${YEL}Traceroute finished ${NC}"
     echo "}" >> $2/dotFile
     xdot $2/dotFile
+    sudo rm -r $2/.tmp/
     else
         echo -e "${RED}You have to precise the host list(csv file) as the first argument and the file name in which you will save as a second argument${NC}"
     fi
